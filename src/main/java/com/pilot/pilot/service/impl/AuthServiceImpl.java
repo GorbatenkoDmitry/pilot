05eb67db89667471a9492a587a8cf1ca8a9924c6
DGorbatenko <dima.gorbatenko.1998@mail.ru>
2024-07-26 20:48:32 +0300

    26/07/2024

--- src/main/java/com/pilot/pilot/service/impl/AuthServiceImpl.java
+++ src/main/java/com/pilot/pilot/service/impl/AuthServiceImpl.java
@@ -1,19 +1,63 @@
 package com.pilot.pilot.service.impl;
 
+import com.pilot.pilot.domain.user.User;
 import com.pilot.pilot.service.AuthService;
+import com.pilot.pilot.service.UserService;
 import com.pilot.pilot.web.dto.auth.JwtRequest;
 import com.pilot.pilot.web.dto.auth.JwtResponse;
+import com.pilot.pilot.web.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
-
+//когда переходим сюда в логин,то мы передаем пароль и логин,юзер нейм пассворд и аутенфикатионтокену и аутентификатион менеджеру
+//они пытаются достать с базы с помощью jwtuserdeteilsservice пользователя с базы, там  у нас метод loadbyusername
+//возвращается спринговый пользователь с помощью нашего фактори и уже у него достается пароль с помощью бкрипта он хэшируется
+//затем сравниваются два пароля
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+   // Метод authenticate(Authentication authentication) интерфейса AuthenticationManager — проверка pароля
+   // Допустим, нам приходит HTTP-запрос. Прежде чем попасть в контроллер, запрос проходит через
+   // цепочку фильтров. В UsernamePasswordAuthenticationFilter имя и пароль вытаскиваются из запроса.
+   // Дальше надо их сравнить с реальными. Тут то вступает в дело AuthenticationManager:
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;//тут у нас все методы по пользователю
+    private final JwtTokenProvider jwtTokenProvider;//там методы по созданию и обновлению токенов
+//переропределяем метод логин
+    //передаем JwtRequest с паролем и логином помеченные анатоцацией,что не могут быть пустые
     @Override
-    public JwtResponse login(JwtRequest loginRequest) {
-        return null;
+    public JwtResponse login(final JwtRequest loginRequest) {
+        //создали объект у него поля айди логин пароль и токины
+        JwtResponse jwtResponse = new JwtResponse();
+        // у authenticationManager вызывли метод authenticate
+        //Допустим, нам приходит HTTP-запрос. Прежде чем попасть в контроллер, запрос проходит через
+        //  цепочку фильтров. В UsernamePasswordAuthenticationFilter имя и пароль вытаскиваются из запроса.
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        //лотсали данные
+                        loginRequest.getUsername(), loginRequest.getPassword())
+        );
+        // если авторизациф прошла можн по юзер нейму поискать через юзер сервис пользователяя
+        User user = userService.getByUsername(loginRequest.getUsername());
+        //засеттили данные
+        jwtResponse.setId(user.getId());
+        jwtResponse.setUsername(user.getUsername());
+        // засетили созданные с помощью jwtTokenProvider.createAccessToken createRefreshToken токин
+        jwtResponse.setAccessToken(jwtTokenProvider.createAccessToken(
+                user.getId(), user.getUsername(), user.getRoles())
+        );
+                jwtResponse.setRefreshToken(jwtTokenProvider.createRefreshToken(
+                user.getId(), user.getUsername())
+        );
+        return jwtResponse;
     }
 
     @Override
-    public JwtResponse refresh(String refreshToken) {
-        return null;
+    public JwtResponse refresh(
+            final String refreshToken
+    ) {
+        return jwtTokenProvider.refreshUserTokens(refreshToken);
     }
+
 }
--- src/main/java/com/pilot/pilot/web/controller/AuthController.java
+++ src/main/java/com/pilot/pilot/web/controller/AuthController.java
@@ -36,29 +36,51 @@ import org.springframework.web.bind.annotation.RestController;
 )
 public class AuthController {
 
-    private final AuthService authService;
-    private final UserService userService;
-    private final UserMapper userMapper;
-//принимаем запрос по клику из формы на путь логин и возвращает JwtResponse? он хранит юзернейм,аксесс и рефреш токен
+    private final AuthService authService;//сервис, который содержит две имплементации
+    // JwtResponse login(JwtRequest loginRequest);
+   // JwtResponse refresh(String refreshToken);
+    private final UserService userService;// сервис, в котором методы для работы с данынми пользователя
+    private final UserMapper userMapper;//мэппер,в котором два метода
+    // UserDto toDto(User user); первому пердается сущность(объект юзер) он трансформируетс в data transfer object
+  //  User toEntity(UserDto dto); второй метод наоборот
+
+
+//далее принимаем запрос по клику из формы на путь логин и возвращает JwtResponse?
+// он хранит юзернейм,аксесс и рефреш токен
     @PostMapping("/login")
     public JwtResponse login(
+            //Идея Bean Validation в том, чтобы определять такие правила,
+            // как «Это поле не может быть null» или «Это число должно находиться в заданном диапазоне» с помощью аннотаций.
+            // Это гораздо проще, чем постоянно писать условные операторы проверок.
+            //Аннотация @RequestBody осуществляет преобразование входящего JSON/XML в объект Java,
+            // что актуально преимущественно для POST-запросов
             @Validated @RequestBody final JwtRequest loginRequest
     ) {
+        //возвращаем метод логин у authService и передаем в параметры loginRequest
         return authService.login(loginRequest);
     }
 
+    //  по аналогии создадим логикиу регистрации
     @PostMapping("/register")
     public UserDto register(
+            //здесь уже есть On crate class
             @Validated(OnCreate.class)
-            @RequestBody final UserDto userDto
+            @RequestBody final UserDto userDto//в юзер дто у нас есть поля ади, имя , пароль и они
+            //все почена как OnCreate.class (в пеерводе вроде при создании чего либо)
+            //выходит получается проверить на валидацию  поля помеченные OnCreate
+
     ) {
         User user = userMapper.toEntity(userDto);
+        //после того как из дто перевели в ентиту,то передали уже методу create в классе userService
         User createdUser = userService.create(user);
         return userMapper.toDto(createdUser);
     }
 
     @PostMapping("/refresh")
+    
     public JwtResponse refresh(
+            //Аннотация @RequestBody осуществляет преобразование входящего JSON/XML в объект Java,
+            // что актуально преимущественно для POST-запросов
             @RequestBody final String refreshToken
     ) {
         return authService.refresh(refreshToken);
